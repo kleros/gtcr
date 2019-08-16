718e226d1f4e173d07898d94fc8f31f4403d467e
Matheus A <malencar@pm.me>
2019-08-16 18:33:48 -0300

    feat: move findItem example to GTCRMock

--- contracts/GTCRMock.sol
+++ contracts/GTCRMock.sol
@@ -11,10 +11,16 @@ pragma experimental ABIEncoderV2;
 
 import { IArbitrable, Arbitrator } from "@kleros/erc-792/contracts/Arbitrator.sol";
 import { IEvidence } from "@kleros/erc-792/contracts/erc-1497/IEvidence.sol";
+
+import "solidity-bytes-utils/contracts/BytesLib.sol";
+import "solidity-rlp/contracts/RLPReader.sol";
 import "./libraries/CappedMath.sol";
 
 contract GTCRMock is IArbitrable, IEvidence{
     using CappedMath for uint; // Operations bounded between 0 and 2**256 - 1.
+    using RLPReader for RLPReader.RLPItem;
+    using RLPReader for bytes;
+    using BytesLib for bytes;
 
     /* Enums */
 
@@ -405,6 +411,36 @@ contract GTCRMock is IArbitrable, IEvidence{
             result.hasPaid = round.hasPaid;
             result.paidFees = round.paidFees;
         }
+    }
+
+    /** @dev Find an item by matching column values.
+     *  - Example:
+     *  Item [18, 'PNK', 'Pinakion', '0xca35b7d915458ef540ade6068dfe2f44e8fa733c']
+     *  RLP encoded: 0xe383504e4b128850696e616b696f6e94ca35b7d915458ef540ade6068dfe2f44e8fa733c
+     *  Input for remix: ["0xe3","0x83","0x50","0x4e","0x4b","0x12","0x88","0x50","0x69","0x6e","0x61","0x6b","0x69","0x6f","0x6e","0x94","0xca","0x35","0xb7","0xd9","0x15","0x45","0x8e","0xf5","0x40","0xad","0xe6","0x06","0x8d","0xfe","0x2f","0x44","0xe8","0xfa","0x73","0x3c"]
+     *  @param _rlpEncodedMatch The RLP encoded item to match against the items on the list.
+     *  @param _cursor The index from where to start looking for matches.
+     *  @param _returnCount The size of the array to return with matching values.
+     *  @param _count The number of items to iterate while searching.
+     *  @return An array with items that match the query.
+     */
+    function findItem(bytes memory _rlpEncodedMatch, uint _cursor, uint _returnCount, uint _count) public view returns (bytes[] memory) {
+        RLPReader.RLPItem[] memory matchItem = _rlpEncodedMatch.toRlpItem().toList();
+        bytes[] memory results = new bytes[](_count);
+        uint itemsFound;
+
+        for(uint i = _cursor; i < (_count == 0 ? itemIDs.length : _count); i++) { // Iterate over every item in storage.
+            bytes storage itemBytes = items[itemIDs[i]].data;
+            RLPReader.RLPItem[] memory item = itemBytes.toRlpItem().toList();
+            for (uint j = 0; j < matchItem.length; j++) { // Iterate over every column.
+                if (item[j].toBytes().equal(matchItem[j].toBytes())) {
+                    results[itemsFound] = itemBytes;
+                    itemsFound++;
+                    break;
+                }
+            }
+        }
+        return results;
     }
 
     /** @dev Return the values of the items the query finds. This function is O(n), where n is the number of items. This could exceed the gas limit, therefore this function should only be used for interface display and not by other contracts.
