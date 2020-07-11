9be328c2de8fe8c34a59b6ce168c39127f632e8a
mtsalenc <mtsalenc@gmail.com>
2020-07-11 19:44:33 -0300

    fix: incorrect loser remaining time display

--- src/hooks/appeal-time.js
+++ src/hooks/appeal-time.js
@@ -1,14 +1,22 @@
 import { useMemo } from 'react'
 import { DISPUTE_STATUS } from '../utils/item-status'
+import { bigNumberify } from 'ethers/utils'
 
 const useAppealTime = item =>
   useMemo(() => {
     if (!item || item.disputeStatus !== DISPUTE_STATUS.APPEALABLE) return {}
-    const { appealEnd } = item
-    const appealRemainingTime = appealEnd.toNumber() * 1000 - Date.now()
+    const { appealStart, appealEnd } = item
+    const appealDuration = appealEnd.sub(appealStart)
+    const appealEndLoser = appealStart.add(appealDuration.div(bigNumberify(2)))
+
+    const appealRemainingTime =
+      appealEnd.toNumber() * 1000 - Math.floor(Date.now())
+    const appealRemainingTimeLoser =
+      appealEndLoser.toNumber() * 1000 - Math.floor(Date.now())
+
     return {
       appealRemainingTime,
-      appealRemainingTimeLoser: appealRemainingTime / 2
+      appealRemainingTimeLoser
     }
   }, [item])
 
