c8d765bd62ced4f030f0bb85cfce28cf7b3f1a61
Green <40367733+greenlucid@users.noreply.github.com>
2022-05-18 21:57:47 +0100

    fix: eth address in display links to etherscan

--- src/components/eth-address.js
+++ src/components/eth-address.js
@@ -3,9 +3,11 @@ import React from 'react'
 import { useWeb3Context } from 'web3-react'
 import { getAddressPage } from '../utils/network-utils'
 
-const ETHAddress = ({ address }) => {
+const ETHAddress = ({ address, forceEth }) => {
   const { networkId } = useWeb3Context()
-  const fullPage = getAddressPage({ networkId, address })
+  const fullPage = forceEth
+    ? `https://etherscan.io/address/${address}`
+    : getAddressPage({ networkId, address })
   return (
     <a href={fullPage} style={{ textDecoration: 'underline' }}>
       {address.slice(0, 6)}...{address.slice(address.length - 4)}
