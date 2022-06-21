985e79ff664268550819116b5bccad7757f048f4
devofficer <nikolaytoplev@gmail.com>
2022-06-22 08:51:41 +1000

    refactor: converting use-light-tcr-context to typescript

--- src/utils/constants/subgraph.ts
+++ src/utils/constants/subgraph.ts
@@ -1,11 +1,15 @@
-export const ITEM_STATUS_CODES = Object.freeze({
+export const ITEM_STATUS_CODES: {
+  [key: string]: number
+} = Object.freeze({
   Absent: 0,
   Registered: 1,
   RegistrationRequested: 2,
   ClearingRequested: 3
 })
 
-export const RULING_CODES = Object.freeze({
+export const RULING_CODES: {
+  [key: string]: number
+} = Object.freeze({
   None: 0,
   Accept: 1,
   Reject: 2
