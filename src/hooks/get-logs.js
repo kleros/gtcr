9ee30ac5806ef3c3dea6daa6ed20b05d9618dc7d
Green <40367733+greenlucid@users.noreply.github.com>
2022-09-19 17:57:00 +0200

    fix: stop overriding library for getLogs

--- src/hooks/get-logs.js
+++ src/hooks/get-logs.js
@@ -1,18 +1,22 @@
-import { ethers } from 'ethers'
 import { useMemo } from 'react'
 
+// once upon a time, infura was giving timeout errors.
+// alchemy was used as a result. but users tend to use
+// metamask, which defaults to infura as rpc provider,
+// and thus these errors kept happening.
+// the solution then was to override the library used to
+// fetch the logs, and use the once provided in the env.
+
+// alchemy is now giving 429 errors a bit too frequently,
+// and infura somehow works seamlessly again, so this function
+// doesn't seem to do anything anymore. but, it's there in case
+// these overrides come useful again. plus, refactoring it out
+// would be time consuming.
 const useGetLogs = library => {
-  const key = JSON.parse(process.env.REACT_APP_RPC_URLS)[1]
-  const provider = new ethers.providers.JsonRpcProvider(key)
   const getLogs = useMemo(
     () => async query => {
-      if (library.network.chainId === 1) {
-        const mainnetResult = await provider.getLogs(query)
-        return mainnetResult
-      } else {
-        const defResult = await library.getLogs(query)
-        return defResult
-      }
+      const defResult = await library.getLogs(query)
+      return defResult
     },
     // eslint-disable-next-line react-hooks/exhaustive-deps
     [library, library.network]
