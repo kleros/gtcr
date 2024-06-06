49cde07c09cf2cb78e9d0935f97c9b68e6e8b4f3
Harman-singh-waraich <Waraichharman68@gmail.com>
2024-06-06 19:50:14 +0530

    chore: point-upload-to-ipfs-function-to-court-function

--- src/utils/get-ipfs-path.ts
+++ src/utils/get-ipfs-path.ts
@@ -3,4 +3,12 @@ export interface IPFSResultObject {
 }
 
 export const getIPFSPath = (ipfsResultObject: IPFSResultObject): string =>
-  `/ipfs/${ipfsResultObject.cids[0].split('ipfs://')[1]}`
+  getFormattedPath(ipfsResultObject.cids[0])
+
+/**
+ *
+ * @param url an ipfs cid
+ * @returns formats an ipfs cid to be in /ipfs/hash format, reolaces ipfs://, ipfs/, with /ipfs/
+ */
+export const getFormattedPath = (url: string) =>
+  url.replace(/^(ipfs:\/\/|ipfs\/?)/, '/ipfs/')
--- src/utils/upload-form-data-to-ipfs.ts
+++ src/utils/upload-form-data-to-ipfs.ts
@@ -1,21 +1,23 @@
 import { fetch } from 'cross-fetch'
 
-export function uploadFormDataToIPFS(
+export async function uploadFormDataToIPFS(
   formData: FormData,
-  operation: string = 'evidence'
+  operation: string = 'evidence',
+  pinToGraph = false
 ): Promise<Response> {
-  const url = `/.netlify/functions/uploadToIPFS?dapp=curate&key=curate-v1&operation=${operation}`
+  const url = `${process.env.REACT_APP_COURT_FUNCTIONS_URL}/.netlify/functions/upload-to-ipfs?operation=${operation}&pinToGraph=${pinToGraph}`
 
-  return fetch(url, {
+  const response = await fetch(url, {
     method: 'POST',
     body: formData
-  }).then(async response => {
-    if (response.status !== 200) {
-      const error = await response
-        .json()
-        .catch(() => ({ message: 'Error uploading to IPFS' }))
-      throw new Error(error.message)
-    }
-    return response
   })
+
+  if (response.status !== 200) {
+    const error = await response
+      .json()
+      .catch(() => ({ message: 'Error uploading to IPFS' }))
+    throw new Error(error.message)
+  }
+  const data = await response.json()
+  return data
 }
