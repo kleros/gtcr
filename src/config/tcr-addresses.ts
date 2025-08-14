c1065277668e631165016c06f36092002d487f59
kemuru <102478601+kemuru@users.noreply.github.com>
2025-08-14 13:19:15 +0200

    feat: add mainnet perma contracts

--- src/config/tcr-addresses.ts
+++ src/config/tcr-addresses.ts
@@ -13,7 +13,7 @@ export const defaultEvidenceDisplayUri = {
 } as const
 
 export const defaultEvidenceDisplayUriPermanent = {
-  // '1': '/ipfs/QmQjJio59WkrQDzPC5kSP3EiGaqrWxjGfkvhmD2mWwm41M/index.html',
+  '1': '/ipfs/QmbNJRDrrd5r9cHeApTP7pZaM7jgacDFMLED5Lj1RJMn79/index.html',
   '100': '/ipfs/QmbNJRDrrd5r9cHeApTP7pZaM7jgacDFMLED5Lj1RJMn79/index.html'
   // '11155111': '/ipfs/QmQjJio59WkrQDzPC5kSP3EiGaqrWxjGfkvhmD2mWwm41M/index.html'
 } as const
@@ -58,7 +58,7 @@ export const lgtcrFactoryAddresses = {
 } as const
 
 export const pgtcrFactoryAddresses = {
-  // '1': '0xe9dd523600b74b8ef0af164687079a6c437f9cd5',
+  '1': '0x69816b499b0ed9a60ac52cf2beb24827e5f13a89',
   '100': '0x75c0406311bb4C81988602b0c209987188DC5A93'
   // '11155111': '0xcB4B48d2A7a44247A00048963F169d2b4Ab045a6'
 } as const
@@ -152,7 +152,7 @@ export const subgraphUrl = {
 } as const
 
 export const subgraphUrlPermanent = {
-  // '1': process.env.REACT_APP_SUBGRAPH_MAINNET,
+  '1': process.env.REACT_APP_SUBGRAPH_MAINNET_PERMANENT,
   '100': process.env.REACT_APP_SUBGRAPH_GNOSIS_PERMANENT
   // '11155111': process.env.REACT_APP_SUBGRAPH_SEPOLIA
 } as const
